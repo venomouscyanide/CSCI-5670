0b6518bc9a9d8b2693ceda2d9fec685800b84abf
Paul Louis <iampaullouis@gmail.com>
2022-03-02 20:11:56 -0500

    Add more stats

--- Assignment1/info_stats.py
+++ Assignment1/info_stats.py
@@ -1,9 +1,14 @@
+import operator
 from collections import defaultdict
 
 import networkx as nx
-from networkx import density, average_clustering, average_shortest_path_length, diameter
+import pandas as pd
+from networkx import density, average_clustering, average_shortest_path_length, diameter, degree_centrality, \
+    closeness_centrality, betweenness_centrality, pagerank
 
 import matplotlib.pyplot as plt
+from networkx.algorithms.community import greedy_modularity_communities, girvan_newman
+import networkx.algorithms.community as nx_comm
 
 
 def get_degree(graph):
@@ -62,6 +67,44 @@ def connected_component_analysis(graph):
     print(f"diameter_scc: {diameter_wcc}", end='\n \n')
 
 
+def community_analysis(graph):
+    # add breakpoints and anylyze the communities in-memory
+    # greedy modularity based
+    greedy_communities = greedy_modularity_communities(graph)
+    gm_communities = girvan_newman(graph)
+    # lovain_communities = nx_comm.louvain_communities(graph) # this is broken
+
+
+def _centrality_helper(type, cent_dict, df, top):
+    top_bottom_10 = list(sorted(cent_dict.items(), key=operator.itemgetter(1), reverse=True))
+    if top:
+        top_bottom_10 = top_bottom_10[:10]
+    else:
+        top_bottom_10 = top_bottom_10[::-1][:10]
+    start = 1 if top else len(cent_dict) - 9
+    for index, (label_value) in enumerate(top_bottom_10, start=start):
+        df.loc[len(df)] = [index, type, round(label_value[1], 5), label_value[0]]
+
+
+def get_top_least_centrality_nodes(graph, name, top=True):
+    df = pd.DataFrame(
+        columns=["Rank", "Type", "Centrality Value", "Node Label"])
+
+    dc = degree_centrality(graph)
+    _centrality_helper("Degree Centrality", dc, df, top)
+
+    cc = closeness_centrality(graph)
+    _centrality_helper("Closeness Centrality", cc, df, top)
+
+    bc = betweenness_centrality(graph)
+    _centrality_helper("Betweeness Centrality", bc, df, top)
+
+    pr = pagerank(graph)
+    _centrality_helper("Page Rank", pr, df, top)
+
+    df.to_csv(name, index=False)
+
+
 def get_stats(graph):
     num_nodes = get_degree(graph)  # no of nodes in the graph
     print(f"Number of nodes: {num_nodes}", end='\n \n')
@@ -78,9 +121,13 @@ def get_stats(graph):
     plot_deg_dist(log_log=True)
 
     connected_component_analysis(graph)
+    community_analysis(graph)
+
+    get_top_least_centrality_nodes(graph, name="df_top.csv", top=True)
+    get_top_least_centrality_nodes(graph, name="df_bottom.csv", top=False)
 
 
 if __name__ == '__main__':
     # graph = nx.read_gexf('final_100000_len_graph.gexf')
-    graph = nx.read_gexf('interim_50000_len_graph_old.gexf')  # smaller graph
+    graph = nx.read_gexf('interim_5000_len_graph_old.gexf')  # smaller graph
     get_stats(graph)
